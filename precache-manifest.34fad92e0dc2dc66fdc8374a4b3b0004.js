5a14a4dd50c8af39feff40c5fbbeeaeb327f0fdb
Justin <justin.leung.ch@gmail.com>
2020-10-11 17:56:03 -0400

    Updates

---
+++ precache-manifest.34fad92e0dc2dc66fdc8374a4b3b0004.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cbbdd1c3c78da4997713a935c71938f",
+    "url": "/visoeis/index.html"
+  },
+  {
+    "revision": "ac2e2174000cd2486620",
+    "url": "/visoeis/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8c6c70d276cf2b3f9466",
+    "url": "/visoeis/static/js/2.10646dad.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/visoeis/static/js/2.10646dad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac2e2174000cd2486620",
+    "url": "/visoeis/static/js/main.187823fe.chunk.js"
+  },
+  {
+    "revision": "591a572dd856d47a203b",
+    "url": "/visoeis/static/js/runtime-main.c30edb0f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.655661d04a2a37a86dd1ca232da0a1e2.js"
+  "/visoeis/precache-manifest.34fad92e0dc2dc66fdc8374a4b3b0004.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/visoeis/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
