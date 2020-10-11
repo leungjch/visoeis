fd7971b706ff501af9c38a627c9737fd2c7a54c7
Justin <justin.leung.ch@gmail.com>
2020-10-11 00:43:28 -0400

    Fix OEIS fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import $ from 'jquery'
 function App() {
+
+
+  // Parses and cleans the b-file .txt
+  // Returns two lists xList and yList
+  function parseData(text : string){
+    
+    let xList : number[];
+    let yList : number[]; 
+
+    return {x: xList, y: yList};
+  }
+  function fetchOEIS() {
+  // Request data from OEIS
+  // Pad the beginning with zeros
+  let seq : string = String(Math.floor(Math.random()*340000)).padStart(6, '0');
+  $.get( `http://oeis.org/A${seq}/b${seq}.txt`, function( data ) {
+    let text : string = data;
+    console.log(text)
+    
+    return parseData(text);
+  });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +33,8 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn
-        </a>
+
+        <button onClick={fetchOEIS}> New sequence! </button>
       </header>
     </div>
   );
